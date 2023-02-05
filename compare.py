bae4c79805f149e5b0e439073bd435fef2d885fd
mysh <mysh995@gmail.com>
2023-02-06 00:22:31 +0100

    Comparing files: compare curves

--- compare.py
+++ compare.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 from dlisio import dlis
 
@@ -31,13 +32,27 @@ ATTRIBUTES = [
 ]
 
 
+def compare_arrays(arr1, arr2):
+    if arr1.shape != arr2.shape:
+        return False
+
+    return ((arr1 == arr2) | (np.isnan(arr1) & np.isnan(arr2))).all()
+
+
 def compare(f1, f2):
     for attribute in ATTRIBUTES:
+        print(f"Checking attribute '{attribute}'")
         if getattr(f1, attribute) != getattr(f2, attribute):
             print(f'Comparison returned False for attribute "{attribute}"')
             return False
-    else:
-        return True
+
+    nc = len(f1.channels)
+    for i in range(nc):
+        print(f'Checking channel {i+1}/{nc}: {f1.channels[i]}')
+        if not compare_arrays(f1.channels[i].curves(), f2.channels[i].curves()):
+            return False
+
+    return True
 
 
 if __name__ == '__main__':
