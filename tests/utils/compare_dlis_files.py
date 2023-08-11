c9c8f4f81ba5a6266604b6c7c61bdb7961e23809
mysh <mysh995@gmail.com>
2023-08-11 13:41:55 +0200

    Script to compare dlis files at binary level

---
+++ tests/utils/compare_dlis_files.py
@@ -0,0 +1,28 @@
+def read_binary(fname):
+    with open(fname, 'rb') as f:
+        contents = f.read()
+    return contents
+
+def compare(f1, f2):
+    data1 = read_binary(f1)
+    data2 = read_binary(f2)
+
+    if len(data1) != len(data2):
+        print("Lengths of the files don't match")
+        return False
+
+    nonmatching_indices = [i for i in range(len(data1)) if data1[i] != data2[i]]
+    if nonmatching_indices:
+        print(f"Files do not match at indices: {nonmatching_indices}")
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if (na := len(argv)) != 3:
+        raise ValueError(f"Expected 2 arguments (names of files to compare), got {na-1}")
+
+    equal = compare(argv[1], argv[2])
+    print(f"Files are {'' if equal else 'not '}equal")
