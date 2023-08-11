9853578d3dbf081280726c81496ece0a8f858a97
mysh <mysh995@gmail.com>
2023-08-11 13:04:26 +0200

    Arg parser for mock hdf5 dataset creation

--- tests/utils/make_mock_data_hdf5.py
+++ tests/utils/make_mock_data_hdf5.py
@@ -1,26 +1,34 @@
 import numpy as np
 import h5py
-from sys import argv
 from pathlib import Path
+from argparse import ArgumentParser
 
 
-if len(argv) > 1:
-    n_points = int(float(argv[1]))
-else:
-    n_points = int(5e4)
+def create_data_file(n_points, fpath):
+    if fpath.exists():
+        raise RuntimeError(f"File '{fpath}' already exists. Cannot overwrite file.")
 
-fpath = Path(__file__).resolve().parent.parent/'resources/mock_data.hdf5'
+    h5_file = h5py.File(fpath, 'w')
+    group = h5_file.create_group('contents')
+    group.create_dataset('time', (n_points,), float, np.arange(n_points))
+    group.create_dataset('depth', (n_points,), float, 10 * (np.arange(n_points) % 5))
+    group.create_dataset('rpm', (n_points,), float, 10 * np.sin(np.linspace(0, 1e4*np.pi, n_points)))
 
-if fpath.exists():
-    raise RuntimeError(f"File '{fpath}' already exists. Cannot overwrite file.")
+    h5_file.flush()
+    h5_file.close()
 
-h5_file = h5py.File(fpath, 'w')
-group = h5_file.create_group('contents')
-group.create_dataset('time', (n_points,), float, np.arange(n_points))
-group.create_dataset('depth', (n_points,), float, 10 * (np.arange(n_points) % 5))
-group.create_dataset('rpm', (n_points,), float, 10 * np.sin(np.linspace(0, 1e4*np.pi, n_points)))
+    print(f"Fake data with {n_points} points saved to file '{fpath}'")
 
-h5_file.flush()
-h5_file.close()
 
-print(f"Fake data with {n_points} points saved to file '{fpath}'")
+if __name__ == '__main__':
+    parser = ArgumentParser("Creating HFD5 file with mock well data")
+    parser.add_argument('-n', '--n-points', help='Number of data points', type=float, default=5e4)
+    parser.add_argument('-fn', '--file-name', help='Output file name')
+    parser_args = parser.parse_args()
+
+    if (file_name := parser_args.file_name) is None:
+        file_name = 'mock_data.hdf5'
+    if len(Path(file_name).parts) == 1 and not file_name.startswith('./'):
+        file_name = Path(__file__).resolve().parent.parent / 'resources' / file_name
+
+    create_data_file(n_points=int(parser_args.n_points), fpath=file_name)
