82cbe6bef17b0a17d481107e536da457f78ae77b
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-10-10 16:59:29 +0200

    DLISWriter class

--- dlis_writer/writer/mwe_dlis_creation.py
+++ dlis_writer/writer/mwe_dlis_creation.py
@@ -19,88 +19,109 @@ from dlis_writer.writer.utils.compare_dlis_files import compare
 logger = logging.getLogger(__name__)
 
 
-def prepare_config(config_fname, data):
-    config = load_config(config_fname)
-    _add_channel_config(data, config=config)
-    _add_index_config(config=config, depth_based="Channel-depth" in config.sections())
-    return config
+class DLISWriter:
+    default_config_file_name = Path(__file__).resolve().parent/'mwe_mock_config.ini',
 
+    def __init__(self, data, config):
+        self._data = data
+        self._config = config
 
-def _add_channel_config(data: np.ndarray, config: ConfigParser,
-                        repr_code: RepresentationCode = None):
+    @staticmethod
+    def make_config(config_file_name, data):
+        config = load_config(config_file_name)
+        DLISWriter._add_channel_config(data, config=config)
+        DLISWriter._add_index_config(config=config, depth_based="Channel-depth" in config.sections())
+        return config
 
-    for name in data.dtype.names:
-        section = f"Channel-{name}"
-        config.add_section(section)
-        config[section]['name'] = name
-        config[section]['dimension'] = ', '.join(str(i) for i in data[name].shape[1:])
-        if repr_code:
-            config[section]['representation_code'] = str(repr_code.value)
+    @staticmethod
+    def _add_channel_config(data: np.ndarray, config: ConfigParser,
+                            repr_code: RepresentationCode = None):
 
+        for name in data.dtype.names:
+            section = f"Channel-{name}"
+            config.add_section(section)
+            config[section]['name'] = name
+            config[section]['dimension'] = ', '.join(str(i) for i in data[name].shape[1:])
+            if repr_code:
+                config[section]['representation_code'] = str(repr_code.value)
 
-def _add_index_config(config: ConfigParser, depth_based=False):
-    if depth_based:
-        config['Frame'].update({'index_type': 'DEPTH', 'spacing.units': 'm'})
-    else:
-        config['Frame'].update({'index_type': 'TIME', 'spacing.units': 's'})
+    @staticmethod
+    def _add_index_config(config: ConfigParser, depth_based=False):
+        if depth_based:
+            config['Frame'].update({'index_type': 'DEPTH', 'spacing.units': 'm'})
+        else:
+            config['Frame'].update({'index_type': 'TIME', 'spacing.units': 's'})
 
+    @staticmethod
+    def create_data(**kwargs):
+        return create_data(**kwargs)
 
-def make_data_capsule(data: np.ndarray, config, channels: list[Channel] = None) -> FrameDataCapsule:
-    frame = Frame.from_config(config)
-    frame.channels.value = channels or Channel.all_from_config(config)
+    @staticmethod
+    def load_data(input_file_name):
+        return load_hdf5(input_file_name)
 
-    logger.info(f'Preparing frames for {data.shape[0]} rows with channels: '
-                f'{", ".join(c.name for c in frame.channels.value)}')
-    data_capsule = FrameDataCapsule(frame, data)
+    def make_data_capsule(self, channels: list[Channel] = None) -> FrameDataCapsule:
+        frame = Frame.from_config(self._config)
+        frame.channels.value = channels or Channel.all_from_config(self._config)
 
-    return data_capsule
+        logger.info(f'Preparing frames for {self._data.shape[0]} rows with channels: '
+                    f'{", ".join(c.name for c in frame.channels.value)}')
+        data_capsule = FrameDataCapsule(frame, self._data)
 
+        return data_capsule
 
-def prepare_data_array(pargs):
-    if (input_file_name := pargs.input_file_name) is None:
-        data = create_data(
-            int(pargs.n_points),
-            n_images=pargs.n_images,
-            n_cols=pargs.n_cols,
-            depth_based=pargs.depth_based
-        )
-    else:
-        data = load_hdf5(input_file_name)
+    def write_dlis_file(self, dlis_file_name, channels=None):
+        def timed_func():
+            # CREATE THE FILE
+            dlis_file = DLISFile.from_config(self._config)
 
-    return data
+            data_capsule = self.make_data_capsule(channels=channels)
 
+            dlis_file.write_dlis(data_capsule, dlis_file_name)
 
-def write_dlis_file(data, dlis_file_name, config, channels=None):
-    def timed_func():
-        # CREATE THE FILE
-        dlis_file = DLISFile.from_config(config)
+        exec_time = timeit(timed_func, number=1)
+        logger.info(f"DLIS file created in {timedelta(seconds=exec_time)} ({exec_time} seconds)")
 
-        data_capsule = make_data_capsule(data, config=config, channels=channels)
+    @classmethod
+    def make_parser(cls):
+        parser = ArgumentParser("DLIS file creation - minimal working example")
+        pg = parser.add_mutually_exclusive_group()
+        pg.add_argument('-n', '--n-points', help='Number of data points', type=float, default=10e3)
+        pg.add_argument('-ifn', '--input-file-name', help='Input file name')
 
-        dlis_file.write_dlis(data_capsule, dlis_file_name)
+        parser.add_argument('-c', '--config', help="Path to config file specifying metadata")
+        parser.add_argument('-fn', '--file-name', help='Output file name')
+        parser.add_argument('-ref', '--reference-file-name',
+                            help="Another DLIS file to compare the created one against (at binary level)")
+        parser.add_argument('-ni', '--n-images', type=int, default=0,
+                            help='Number of 2D data sets to add (ignored if input file is specified)')
+        parser.add_argument('-nc', '--n-cols', type=int, default=128,
+                            help='No. columns for each of the added 2D data sets (ignored if input file specified)')
+        parser.add_argument('--depth-based', action='store_true', default=False,
+                            help="Make a depth-based HDF5 file (default is time-based)")
 
-    exec_time = timeit(timed_func, number=1)
-    logger.info(f"DLIS file created in {timedelta(seconds=exec_time)} ({exec_time} seconds)")
+        return parser
 
+    @classmethod
+    def from_parser_args(cls, pargs):
+        if (input_file_name := pargs.input_file_name) is None:
+            data = cls.create_data(
+                n_points=int(pargs.n_points),
+                n_images=pargs.n_images,
+                n_cols=pargs.n_cols,
+                depth_based=pargs.depth_based
+            )
+        else:
+            data = cls.load_data(input_file_name)
 
-def make_parser():
-    parser = ArgumentParser("DLIS file creation - minimal working example")
-    pg = parser.add_mutually_exclusive_group()
-    pg.add_argument('-n', '--n-points', help='Number of data points', type=float, default=10e3)
-    pg.add_argument('-ifn', '--input-file-name', help='Input file name')
+        if pargs.config:
+            config = load_config(pargs.config)
+        else:
+            config = cls.make_config(cls.default_config_file_name, data)
 
-    parser.add_argument('-c', '--config', help="Path to config file specifying metadata")
-    parser.add_argument('-fn', '--file-name', help='Output file name')
-    parser.add_argument('-ref', '--reference-file-name',
-                        help="Another DLIS file to compare the created one against (at binary level)")
-    parser.add_argument('-ni', '--n-images', type=int, default=0,
-                        help='Number of 2D data sets to add (ignored if input file is specified)')
-    parser.add_argument('-nc', '--n-cols', type=int, default=128,
-                        help='Number of columns for each of the added 2D data sets (ignored if input file specified)')
-    parser.add_argument('--depth-based', action='store_true', default=False,
-                        help="Make a depth-based HDF5 file (default is time-based)")
+        writer = cls(data, config)
 
-    return parser
+        return writer
 
 
 def compare_files(output_file_name, reference_file_name):
@@ -112,22 +133,24 @@ def compare_files(output_file_name, reference_file_name):
         logger.warning("Files are NOT equal")
 
 
+def write_dlis_file(data, dlis_file_name, config, channels=None):
+    writer = DLISWriter(data, config)
+    writer.write_dlis_file(dlis_file_name=dlis_file_name, channels=channels)
+
+
 if __name__ == '__main__':
     install_logger(logger)
 
-    parser = make_parser()
+    parser = DLISWriter.make_parser()
     parser.set_defaults(
-        config_file_name=Path(__file__).resolve().parent/'mwe_mock_config.ini',
         output_file_name=Path(__file__).resolve().parent.parent/'tests/outputs/mwe_fake_dlis.DLIS'
     )
     pargs = parser.parse_args()
 
     os.makedirs(Path(pargs.output_file_name).parent, exist_ok=True)
 
-    data = prepare_data_array(pargs)
-    cfg = prepare_config(pargs.config_file_name, data=data)
-
-    write_dlis_file(data=data, dlis_file_name=pargs.output_file_name, config=cfg)
+    dlis_writer = DLISWriter.from_parser_args(pargs)
+    dlis_writer.write_dlis_file(dlis_file_name=pargs.output_file_name)
 
     if pargs.reference_file_name is not None:
         compare_files(pargs.output_file_name, pargs.reference_file_name)
