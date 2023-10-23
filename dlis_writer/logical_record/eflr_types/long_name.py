3a46a0d5cd379c2036f894132802b1b5389ea0c8
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-10-23 14:14:37 +0200

    LongName: removed from RP66

--- dlis_writer/logical_record/eflr_types/long_name.py
+++ dlis_writer/logical_record/eflr_types/long_name.py
@@ -1,5 +1,5 @@
 from dlis_writer.logical_record.core import EFLR
-from dlis_writer.utils.enums import LogicalRecordType
+from dlis_writer.utils.enums import LogicalRecordType, RepresentationCode as RepC
 
 
 class LongName(EFLR):
@@ -9,20 +9,26 @@ class LongName(EFLR):
     def __init__(self, name: str, set_name: str = None, **kwargs):
         super().__init__(name, set_name)
 
-        self.general_modifier = self._create_attribute('general_modifier', converter=self.convert_values)
-        self.quantity = self._create_attribute('quantity')
-        self.quantity_modifier = self._create_attribute('quantity_modifier', converter=self.convert_values)
-        self.altered_form = self._create_attribute('altered_form')
-        self.entity = self._create_attribute('entity')
-        self.entity_modifier = self._create_attribute('entity_modifier', converter=self.convert_values)
-        self.entity_number = self._create_attribute('entity_number')
-        self.entity_part = self._create_attribute('entity_part')
-        self.entity_part_number = self._create_attribute('entity_part_number')
-        self.generic_source = self._create_attribute('generic_source')
-        self.source_part = self._create_attribute('source_part', converter=self.convert_values)
-        self.source_part_number = self._create_attribute('source_part_number', converter=self.convert_values)
-        self.conditions = self._create_attribute('conditions', converter=self.convert_values)
-        self.standard_symbol = self._create_attribute('standard_symbol')
-        self.private_symbol = self._create_attribute('private_symbol')
+        self.general_modifier = self._create_attribute(
+            'general_modifier', converter=self.convert_values, representation_code=RepC.ASCII, multivalued=True)
+        self.quantity = self._create_attribute('quantity', representation_code=RepC.ASCII)
+        self.quantity_modifier = self._create_attribute(
+            'quantity_modifier', converter=self.convert_values, representation_code=RepC.ASCII, multivalued=True)
+        self.altered_form = self._create_attribute('altered_form', representation_code=RepC.ASCII)
+        self.entity = self._create_attribute('entity', representation_code=RepC.ASCII)
+        self.entity_modifier = self._create_attribute(
+            'entity_modifier', converter=self.convert_values, representation_code=RepC.ASCII, multivalued=True)
+        self.entity_number = self._create_attribute('entity_number', representation_code=RepC.ASCII)
+        self.entity_part = self._create_attribute('entity_part', representation_code=RepC.ASCII)
+        self.entity_part_number = self._create_attribute('entity_part_number', representation_code=RepC.ASCII)
+        self.generic_source = self._create_attribute('generic_source', representation_code=RepC.ASCII)
+        self.source_part = self._create_attribute(
+            'source_part', converter=self.convert_values, multivalued=True, representation_code=RepC.ASCII)
+        self.source_part_number = self._create_attribute(
+            'source_part_number', converter=self.convert_values, multivalued=True, representation_code=RepC.ASCII)
+        self.conditions = self._create_attribute(
+            'conditions', converter=self.convert_values, multivalued=True, representation_code=RepC.ASCII)
+        self.standard_symbol = self._create_attribute('standard_symbol', representation_code=RepC.ASCII)
+        self.private_symbol = self._create_attribute('private_symbol', representation_code=RepC.ASCII)
 
         self.set_attributes(**kwargs)
