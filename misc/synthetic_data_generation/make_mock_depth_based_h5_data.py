fbfbaecde30811fea08b7f444a60357d7cc292ad
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-10-31 10:14:32 +0100

    Refactor creating depth based synthetic data file

--- misc/synthetic_data_generation/make_mock_depth_based_h5_data.py
+++ misc/synthetic_data_generation/make_mock_depth_based_h5_data.py
@@ -8,18 +8,8 @@ def make_image(n_rows, n_cols, divider=11):
     return (np.arange(n_rows * n_cols) % divider).reshape(n_rows, n_cols) + 5 * np.random.rand(n_rows, n_cols)
 
 
-if __name__ == '__main__':
-    fpath = Path(__file__).parent/'tmp_data/mock_depth_amplitude_radius.hdf5'
-
-    depth_min = 2500
-    depth_span = 100
-    depth_res = 0.1
-
+def make_h5_file(fpath, depth_min, depth_span, depth_res=0.1, n_cols=128):
     n_points = int(depth_span // depth_res + 1)
-    n_cols = 128
-
-    if fpath.exists():
-        os.remove(fpath)
 
     h5_file = h5py.File(fpath, 'w')
     try:
@@ -36,3 +26,12 @@ if __name__ == '__main__':
         h5_file.close()
 
     print(f"Fake data with {n_points} points saved to file '{fpath}'")
+
+
+if __name__ == '__main__':
+    file_path = Path(__file__).parent/'tmp_data/mock_depth_amplitude_radius.hdf5'
+
+    if file_path.exists():
+        os.remove(file_path)
+
+    make_h5_file(file_path, depth_min=2500, depth_span=100, depth_res=0.1, n_cols=128)
