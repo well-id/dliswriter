1e350d7d13e1e6c504171438e1e9427208d21ad9
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-11-16 11:49:52 +0100

    HDF5Interface class with ability to load data chunks

--- dlis_writer/utils/loaders.py
+++ dlis_writer/utils/loaders.py
@@ -5,6 +5,61 @@ import os
 from configparser import ConfigParser
 
 
+class HDF5Interface:
+    def __init__(self, data_file_name: Union[str, bytes, os.PathLike], mapping: dict, **kwargs):
+        # open the HDF5 file and access the data found under the top-level group name
+        self._h5_data = h5py.File(data_file_name, 'r', **kwargs)
+        self._mapping = {k: (f'/{v}' if not v.startswith('/') else v) for k, v in mapping.items()}
+
+        self._dtype = self.determine_dtypes(self._h5_data, self._mapping)
+
+        self._n_rows = self._h5_data.get(next(iter(self._mapping.values()))).shape[0]
+
+    @staticmethod
+    def determine_dtypes(h5_data: h5py.File, mapping: dict):
+        dtypes = []
+        for key, loc in mapping.items():
+            dset = h5_data.get(loc)
+            if not dset:
+                raise ValueError(f"No dataset '{loc}' found in the file")
+            key_data = h5_data.get(loc)[0:1]  # get in form of a 1-element array, not a single number
+
+            # determine the dtype of the data set (2- or 3-tuple)
+            dt = (key, key_data.dtype)
+            if key_data.ndim > 1:
+                if key_data.ndim > 2:
+                    raise RuntimeError("Data sets with more than 2 dimensions are not supported")
+                dt = (*dt, key_data.shape[-1])  # 3-tuple if the data set has multiple samples per row
+            dtypes.append(dt)
+
+        return np.dtype(dtypes)
+
+    def load_chunk(self, start: int, stop: Union[int, None]):
+        idx = slice(start, stop)
+        n_rows = (stop or self._n_rows) - start
+
+        chunk = np.zeros(n_rows, dtype=self._dtype)
+        for key, loc in self._mapping.items():
+            chunk[key] = self._h5_data.get(loc)[idx]
+
+        return chunk
+
+    def close(self):
+        self._h5_data.close()
+
+    def __del__(self):
+        self.close()
+
+    def make_chunked_generator(self, chunk_rows):
+        n_full_chunks, remainder_rows = divmod(self._n_rows, chunk_rows)
+
+        for i in range(n_full_chunks):
+            yield from self.load_chunk(i * chunk_rows, (i + 1) * chunk_rows)
+
+        if remainder_rows:
+            yield from self.load_chunk(n_full_chunks * chunk_rows, None)
+
+
 def load_hdf5(data_file_name: Union[str, bytes, os.PathLike], key: str = 'contents') -> np.ndarray:
     """Load HDF5 data into a structured numpy array.
 
