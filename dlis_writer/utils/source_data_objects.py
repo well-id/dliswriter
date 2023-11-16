8ca03657405031bfe5ad653baed4dee49eb23b0c
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-11-16 15:27:35 +0100

    Moved HDF5Interface to a separate script

---
+++ dlis_writer/utils/source_data_objects.py
@@ -0,0 +1,64 @@
+import numpy as np
+import h5py
+from typing import Union
+import os
+
+
+class HDF5Interface:
+    def __init__(self, data_file_name: Union[str, bytes, os.PathLike], mapping: dict, **kwargs):
+        # open the HDF5 file and access the data found under the top-level group name
+        self._h5_data = h5py.File(data_file_name, 'r', **kwargs)
+        self._mapping = {k: (f'/{v}' if not v.startswith('/') else v) for k, v in mapping.items()}
+
+        self._dtype = self.determine_dtypes(self._h5_data, self._mapping)
+
+        self._n_rows = self._h5_data.get(next(iter(self._mapping.values()))).shape[0]
+
+    @staticmethod
+    def determine_dtypes(data_object, mapping: dict):
+        dtypes = []
+        for dtype_name, dataset_name in mapping.items():
+            try:
+                dset = data_object[dataset_name]
+            except (ValueError, KeyError):
+                raise ValueError(f"No dataset '{dataset_name}' found in the source data")
+            dset_row0 = dset[0:1]  # get in form of a 1-element array, not a single number
+
+            # determine the dtype of the data set (2- or 3-tuple)
+            dt = (dtype_name, dset_row0.dtype)
+            if dset_row0.ndim > 1:
+                if dset_row0.ndim > 2:
+                    raise RuntimeError("Data sets with more than 2 dimensions are not supported")
+                dt = (*dt, dset_row0.shape[-1])  # 3-tuple if the data set has multiple samples per row
+            dtypes.append(dt)
+
+        return np.dtype(dtypes)
+
+    def load_chunk(self, start: int, stop: Union[int, None]):
+        idx = slice(start, stop)
+        n_rows = (stop or self._n_rows) - start
+
+        chunk = np.zeros(n_rows, dtype=self._dtype)
+        for key, loc in self._mapping.items():
+            chunk[key] = self._h5_data.get(loc)[idx]
+
+        return chunk
+
+    def close(self):
+        self._h5_data.close()
+
+    def __del__(self):
+        self.close()
+
+    def make_chunked_generator(self, chunk_rows):
+        if chunk_rows is None:
+            n_full_chunks = 1
+            remainder_rows = 0
+        else:
+            n_full_chunks, remainder_rows = divmod(self._n_rows, chunk_rows)
+
+        for i in range(n_full_chunks):
+            yield from self.load_chunk(i * chunk_rows, (i + 1) * chunk_rows)
+
+        if remainder_rows:
+            yield from self.load_chunk(n_full_chunks * chunk_rows, None)
