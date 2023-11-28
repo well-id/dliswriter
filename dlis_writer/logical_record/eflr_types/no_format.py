9835599ea24e9abd4540dc50963f37d05b4f7993
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-11-28 16:46:48 +0100

    [Docstrings] NoFormat

--- dlis_writer/logical_record/eflr_types/no_format.py
+++ dlis_writer/logical_record/eflr_types/no_format.py
@@ -4,8 +4,16 @@ from dlis_writer.logical_record.core.attribute import Attribute
 
 
 class NoFormatObject(EFLRObject):
+    """Model an object being part of NoFormat EFLR."""
 
     def __init__(self, name: str, parent: "NoFormat", **kwargs):
+        """Initialise NoFormatObject.
+
+        Args:
+            name        :   Name of the NoFormatObject.
+            parent      :   NoFormat EFLR instance this NoFormatObject belongs to.
+            **kwargs    :   Values of to be set as characteristics of the NoFormatObject Attributes.
+        """
 
         self.consumer_name = Attribute('consumer_name', representation_code=RepC.IDENT, parent_eflr=self)
         self.description = Attribute('description', representation_code=RepC.ASCII, parent_eflr=self)
@@ -14,6 +22,8 @@ class NoFormatObject(EFLRObject):
 
 
 class NoFormat(EFLR):
+    """Model NoFormat EFLR."""
+    
     set_type = 'NO-FORMAT'
     logical_record_type = EFLRType.UDI
     object_type = NoFormatObject
