79c65dca33bedde2157dc01be8880eabbf91cc30
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-11-28 13:46:56 +0100

    [Docstrings] LRMeta and LogicalRecord

--- dlis_writer/logical_record/core/logical_record/logical_record.py
+++ dlis_writer/logical_record/core/logical_record/logical_record.py
@@ -11,33 +11,58 @@ logger = logging.getLogger(__name__)
 
 
 class LRMeta(type):
+    """Define a metaclass for all logical records.
+
+    This metaclass adds a class-level property 'lr_type_struct' to LogicalRecord. This allows the value to be computed
+    once for each LogicalRecord subclass.
+    """
+
     def __new__(cls, *args, **kwargs):
+        """Create a new LogicalRecord (sub)class.
+
+        Add a class-level attribute _lr_type_struct to be set when the value is first accessed through the corresponding
+        property.
+        """
+
         obj = super().__new__(cls, *args, **kwargs)
         obj._lr_type_struct = None
         return obj
 
     @property
-    def lr_type_struct(cls):
+    def lr_type_struct(cls) -> bytes:
+        """Bytes describing the type of the logical record."""
+
         if not cls._lr_type_struct:
             cls._lr_type_struct = write_struct(RepresentationCode.USHORT, cls.logical_record_type.value)
         return cls._lr_type_struct
 
 
 class LogicalRecord(metaclass=LRMeta):
-    """Base for all logical record classes."""
+    """Create a base for all logical record classes."""
 
-    is_eflr = NotImplemented
-    logical_record_type: Union[EFLRType, IFLRType] = NotImplemented
+    is_eflr = NotImplemented  #: indication whether this is an explicitly or indirectly formatted LR
+    logical_record_type: Union[EFLRType, IFLRType] = NotImplemented  #: int-enum denoting type of the logical record
 
     def __init__(self, *args, **kwargs):
+        """Initialise a LogicalRecord."""
+
         pass
 
     @abstractmethod
     def _make_body_bytes(self) -> bytes:
+        """Create bytes describing the body of this LogicalRecord.
+
+        See the implementations subclasses: EFLR, FrameData (IFLR), and NoFormatFrameData (IFLR).
+        """
+
         pass
 
     def represent_as_bytes(self) -> LogicalRecordBytes:
-        """Writes bytes of the entire Logical Record Segment that is an EFLR object"""
+        """Create bytes representing the LogicalRecord.
+
+        The bytes are wrapped in LogicalRecordBytes object which facilitates dividing the array of bytes into segments
+        which can fit into visible records (physical units of DLIS).
+        """
 
         return LogicalRecordBytes(
             self._make_body_bytes(),
