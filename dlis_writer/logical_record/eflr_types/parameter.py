9bda063fb42d715ddc0df1ecf31acad13d607370
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-11-28 17:02:32 +0100

    [Docstrings] Parameter

--- dlis_writer/logical_record/eflr_types/parameter.py
+++ dlis_writer/logical_record/eflr_types/parameter.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Union
 
 from dlis_writer.logical_record.core.eflr import EFLR, EFLRObject
 from dlis_writer.utils.enums import EFLRType, RepresentationCode as RepC
@@ -11,10 +12,16 @@ logger = logging.getLogger(__name__)
 
 
 class ParameterObject(EFLRObject):
-    set_type = 'PARAMETER'
-    logical_record_type = EFLRType.STATIC
-
+    """Model an object being part of Parameter EFLR."""
+    
     def __init__(self, name: str, parent: "Parameter", **kwargs):
+        """Initialise ParameterObject.
+
+        Args:
+            name        :   Name of the ParameterObject.
+            parent      :   Parameter EFLR instance this ParameterObject belongs to.
+            **kwargs    :   Values of to be set as characteristics of the ParameterObject Attributes.
+        """
 
         self.long_name = Attribute('long_name', representation_code=RepC.ASCII, parent_eflr=self)
         self.dimension = DimensionAttribute('dimension', parent_eflr=self)
@@ -27,12 +34,16 @@ class ParameterObject(EFLRObject):
         self._set_defaults()
 
     def _set_defaults(self):
+        """Set default values of some attributes if no values have been set so far."""
+
         if not self.dimension.value:
             logger.debug(f"Setting dimension of '{self}' to the default value: [1]")
             self.dimension.value = [1]
 
     @classmethod
-    def convert_value(cls, val):
+    def convert_value(cls, val: str) -> Union[str, int, float]:
+        """Try converting a value to a number. If that fails, return the value unchanged."""
+
         try:
             val = cls.convert_numeric(val)
         except ValueError:
@@ -40,20 +51,21 @@ class ParameterObject(EFLRObject):
         return val
 
     @staticmethod
-    def convert_numeric(value):
-        for parser in (int, float):
-            try:
-                value = parser(value)
-                break
-            except ValueError:
-                pass
-        else:
-            # if loop not broken - none of the converters worked
-            raise ValueError(f"Some/all of the values: {value} could not be converted to numeric types")
+    def convert_numeric(value: str) -> Union[int, float]:
+        """Convert a string to an integer or float."""
+
+        parser = float if '.' in value else int
+
+        try:
+            value = parser(value)
+        except ValueError:
+            raise ValueError(f"Value '{value}' could not be converted to a numeric type")
         return value
 
 
 class Parameter(EFLR):
+    """Model Parameter EFLR."""
+
     set_type = 'PARAMETER'
     logical_record_type = EFLRType.STATIC
     object_type = ParameterObject
