770c5e7c01cfad93989867f1b128044f65af28d6
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-11-29 11:58:17 +0100

    [Docstrings] write_struct (tbc); removed ident, replaced with ascii

--- dlis_writer/utils/common.py
+++ dlis_writer/utils/common.py
@@ -11,15 +11,15 @@ ULONG_OFFSET = 3221225472   #: offset added to values packed as ULONG; 11 and 30
 
 
 def _write_struct_dtime(date_time: datetime) -> bytes:
-    """Converts datetime object to RP66 V1 DTIME.
+    """Convert a datetime object to bytes according to the RP66 V1 standard.
 
     Args:
-        date_time: A datetime object.
+        date_time: The datetime object to be converted.
 
     Returns:
-        RP66 V1 DTIME bytes.
+        Bytes representing the date time.
 
-    Quote:
+    From RP66:
         Y = Years Since 1900 (Range 0 to 255)
         TZ = Time Zone (0 = Local Standard, 1 = Local Daylight Savings, 2 = Greenwich Mean Time)
         M = Month of the Year (Range 1 to 12)
@@ -35,18 +35,6 @@ def _write_struct_dtime(date_time: datetime) -> bytes:
         15 seconds past minute, 620 milliseconds past second =
             01010111 00010100 00010011 00010101
             00010100 00001111 00000010 01101100
-
-
-    Example:
-        >>> from datetime import datetime
-        >>> date_time = datetime(2022, 9, 13, 9, 54)
-        >>> date_time
-        datetime.datetime(2022, 9, 13, 9, 54)
-        >>> _write_struct_dtime(date_time)
-        b'z\t\r\t6\x00\x00\x00'
-
-    .. _RP66 V1 Appendix B.21:
-        http://w3.energistics.org/rp66/v1/rp66v1_appb.html#B_21
 
     """
     
@@ -66,12 +54,19 @@ def _write_struct_dtime(date_time: datetime) -> bytes:
     return value
 
 
-def _write_struct_ascii(value):
+def _write_struct_ascii(value: Any) -> bytes:
+    """Convert value to str, encode as ASCII, and represent as bytes.
+
+    The first bytes are the number of characters in the value (converted to str).
+    """
+
     value = str(value)
     return _write_struct_uvari(len(value)) + value.encode('ascii')
 
 
-def _write_struct_uvari(value):
+def _write_struct_uvari(value: int) -> bytes:
+    """Convert an integer to bytes. The format (USHORT/UNORM/ULONG) is chosen depending on the provided value."""
+
     if value < 128:
         return RepresentationCode.USHORT.converter.pack(value)
 
@@ -82,11 +77,6 @@ def _write_struct_uvari(value):
     return RepresentationCode.ULONG.converter.pack(value + ULONG_OFFSET)
 
 
-def _write_struct_ident(value):
-    value = str(value)
-    return RepresentationCode.USHORT.converter.pack(len(value)) + value.encode('ascii')
-
-
 def _write_struct_obname(value):
     if value.origin_reference is None:
         raise RuntimeError(f"Origin reference of {value} has not been specified")
@@ -94,7 +84,7 @@ def _write_struct_obname(value):
     try:
         origin_reference = _write_struct_uvari(value.origin_reference)
         copy_number = RepresentationCode.USHORT.converter.pack(value.copy_number)
-        name = _write_struct_ident(value.name)
+        name = _write_struct_ascii(value.name)
 
         obname = origin_reference + copy_number + name
 
@@ -106,7 +96,7 @@ def _write_struct_obname(value):
 
 
 def _write_struct_objref(value):
-    return _write_struct_ident(value.parent.set_type) + value.obname
+    return _write_struct_ascii(value.parent.set_type) + value.obname
 
 
 def _write_struct_status(value):
@@ -120,7 +110,7 @@ def _write_struct_status(value):
 _struct_dict = {
     RepresentationCode.ASCII: _write_struct_ascii,
     RepresentationCode.UVARI: _write_struct_uvari,
-    RepresentationCode.IDENT: _write_struct_ident,
+    RepresentationCode.IDENT: _write_struct_ascii,
     RepresentationCode.DTIME: _write_struct_dtime,
     RepresentationCode.OBNAME: _write_struct_obname,
     RepresentationCode.OBJREF: _write_struct_objref,
