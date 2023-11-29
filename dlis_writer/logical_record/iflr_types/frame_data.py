25cae9dca86958991f39c7e08ca994b57582222d
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-11-29 11:36:26 +0100

    [Docstrings] FrameData

--- dlis_writer/logical_record/iflr_types/frame_data.py
+++ dlis_writer/logical_record/iflr_types/frame_data.py
@@ -1,41 +1,48 @@
+import numpy as np
+from typing import TYPE_CHECKING, Optional
+
 from dlis_writer.logical_record.core.iflr import IFLR
 from dlis_writer.utils.common import write_struct
 from dlis_writer.utils.enums import RepresentationCode, IFLRType
 
+if TYPE_CHECKING:
+    from dlis_writer.logical_record.eflr_types.frame import FrameObject
+
 
 class FrameData(IFLR):
+    """Model FrameData - an Implicitly Formatted Logical Record meant for storing numerical data.
+
+    Each FrameData record begins with a reference to the FrameObject that it belongs to.
+    """
+
     logical_record_type = IFLRType.FDATA
 
-    def __init__(self, frame, frame_number: int, slots, origin_reference=None):
+    def __init__(self, frame: "FrameObject", frame_number: int, slots: np.ndarray,
+                 origin_reference: Optional[int] = None):
+        """Initialise a FrameData.
 
-        '''
-    
-        FrameData is an Implicitly Formatted Logical Record (IFLR)
-
-        Each FrameData object begins with a reference to the Frame object
-        that they belong to using OBNAME.
-
-        :frame -> A Frame(EFLR) object instance
-        :position -> Auto-assigned integer denoting the position of the FrameData
-        
-        :slots -> Represents a row in the dataframe. If there are 3 channels, slots will be the array
-        of values at the corresponding index for those channels.
-
-        '''
+        Args:
+            frame               :   The frame that the data belongs to.
+            frame_number        :   Index of the frame, i.e. index (starting from 1) of the row of data.
+            slots               :   Data belonging to the object. It should be a row of a structured numpy array
+                                    with consecutive items corresponding to the channels in the frame.
+            origin_reference    :   Origin reference for the object.
+        """
 
         super().__init__()
 
         self._frame = frame
-        self._frame_number = frame_number  # UVARI
-        self._slots = slots  # np.ndarray
+        self._frame_number = frame_number
+        self._slots = slots
 
         self.origin_reference = origin_reference
 
-    @property
-    def frame(self):
-        return self._frame
-
     def _make_body_bytes(self) -> bytes:
+        """Create bytes describing the body of the FrameData object.
+
+        This includes: reference to the parent frame, placement of the FrameData object (the index), and the data.
+        """
+
         body = self._frame.obname + write_struct(RepresentationCode.UVARI, self._frame_number)
 
         for s in self._slots:
