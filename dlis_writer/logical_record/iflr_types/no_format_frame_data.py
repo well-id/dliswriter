2ee59252449d0143a7188e27780fe9439800c1d1
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-11-29 11:38:49 +0100

    [Docstrings] NoFormatFrameData

--- dlis_writer/logical_record/iflr_types/no_format_frame_data.py
+++ dlis_writer/logical_record/iflr_types/no_format_frame_data.py
@@ -3,9 +3,12 @@ from dlis_writer.utils.enums import IFLRType
 
 
 class NoFormatFrameData(IFLR):
+    """Model a NoFormatFrameData - an Indirectly Formatted Logical Record with data in ASCII."""
+
     logical_record_type = IFLRType.NOFMT
 
     def __init__(self):
+        """Initialise NoFormatFrameData."""
 
         super().__init__()
 
@@ -13,8 +16,6 @@ class NoFormatFrameData(IFLR):
         self.data = None
 
     def _make_body_bytes(self) -> bytes:
-        _body = b''
-        _body += self.no_format_object.obname
-        _body += self.data.encode('ascii')
+        """Create bytes representing the body of the object."""
 
-        return _body
+        return self.no_format_object.obname + self.data.encode('ascii')
