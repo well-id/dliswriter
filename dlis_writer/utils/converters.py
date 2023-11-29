be400b6e722e3fc9f1493c2a264747c06e9afd26
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-11-29 12:55:31 +0100

    [Docstrings & Refactoring] get_ascii_bytes

--- dlis_writer/utils/converters.py
+++ dlis_writer/utils/converters.py
@@ -1,30 +1,33 @@
-from functools import lru_cache
 from datetime import datetime
 import numpy as np
 
 from dlis_writer.utils.enums import RepresentationCode
 
 
-@lru_cache(maxsize=4096)
 def get_ascii_bytes(value: str, required_length: int, justify_left: bool = False) -> bytes:
-    """Special method used for Storage Unit Label.
+    """Encode a string value as ASCII.
 
     Args:
-        value:              Value to convert to ascii bytes
-        required_length:    Specified ascii string length
-        justify_left:       When the length of the value is less than required length,
-                                trailing or preceding blanks/zeros is required depending on the data type.
-                                The default behaviour (flag set to False) is justifying the value to the right
-                                and filling the remaining required characters with PRECEDING "blanks".
-                                Setting the flag to True adds TRAILING blanks.
+        value           :   Value to convert.
+        required_length :   Required length of the string. Padding will be added if the original string is shorter.
+        justify_left    :   If True, pad with trailing blanks. Otherwise, pad with preceding blanks.
 
     Returns:
         ASCII encoded bytes.
     """
 
+    lv = len(value)
+    if lv > required_length:
+        raise ValueError(f"Provided string is too long ({lv} chars) for required length of {required_length}")
+
+    padding = (required_length - lv) * ' '
+
     if justify_left:
-        return (str(value) + (required_length - len(str(value))) * ' ').encode('ascii')
-    return ((required_length - len(str(value))) * ' ' + str(value)).encode('ascii')
+        padded_value = value + padding
+    else:
+        padded_value = padding + value
+
+    return padded_value.encode('ascii')
 
 
 class ReprCodeConverter:
