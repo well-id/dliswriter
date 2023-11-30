ea04696cfbca11263591b963e4e2b3ac7e751266
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-11-30 12:15:23 +0100

    [Docstrings] dlis file comparator

--- dlis_writer/writer/dlis_file_comparator.py
+++ dlis_writer/writer/dlis_file_comparator.py
@@ -1,17 +1,35 @@
+import os
 import logging
+from typing import Union
+
 from dlis_writer.utils.logging import install_logger
 
 
 logger = logging.getLogger(__name__)
 
+path_type = Union[str, bytes, os.PathLike]
 
-def read_binary(fname):
+
+def read_binary(fname: path_type) -> bytes:
+    """Read a binary file and return the contained bytes."""
+
     with open(fname, 'rb') as f:
         contents = f.read()
     return contents
 
 
-def compare(f1, f2, verbose=True):
+def compare(f1: path_type, f2: path_type, verbose: bool = True) -> bool:
+    """Compare two files at binary level.
+
+    Args
+        f1      :   Name of the first file.
+        f2      :   Name of the second file.
+        verbose :   If True and the two files don't match, print out the reason (mismatching lengths or contents).
+
+    Returns:
+          True if files are identical; False otherwise.
+    """
+
     data1 = read_binary(f1)
     data2 = read_binary(f2)
 
@@ -20,10 +38,10 @@ def compare(f1, f2, verbose=True):
             print(f"Lengths of the files don't match ({l1} vs {l2})")
         return False
 
-    nonmatching_indices = [i for i in range(len(data1)) if data1[i] != data2[i]]
-    if nonmatching_indices:
+    mismatched_indices = [i for i in range(len(data1)) if data1[i] != data2[i]]
+    if mismatched_indices:
         if verbose:
-            print(f"Files do not match at {len(nonmatching_indices)} indices: {nonmatching_indices}")
+            print(f"Files do not match at {len(mismatched_indices)} indices: {mismatched_indices}")
         return False
     return True
 
