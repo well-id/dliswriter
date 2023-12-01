7cd480fa34e90e56e4a30f3fa132bed9e04b5413
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-12-01 16:30:43 +0100

    [Docstrings] Test StorageUnitLabel

--- dlis_writer/tests/test_logical_record/test_misc/test_storage_unit_label.py
+++ dlis_writer/tests/test_logical_record/test_misc/test_storage_unit_label.py
@@ -1,21 +1,28 @@
 import pytest
+from configparser import ConfigParser
 
 from dlis_writer.logical_record.misc import StorageUnitLabel
 from dlis_writer.tests.common import base_data_path, config_params, make_config
 
 
-def _check_with_config(sul, config):
+def _check_with_config(sul: StorageUnitLabel, config: ConfigParser):
+    """Test that the parameters of the storage unit label are as expected."""
+
     assert sul.set_identifier == config['StorageUnitLabel']['name']
     assert sul.sequence_number == int(config['StorageUnitLabel']['sequence_number'])
 
 
-def test_from_config(config_params):
+def test_from_config(config_params: ConfigParser):
+    """Test creating StorageUnitLabel from config."""
+
     sul = StorageUnitLabel.make_from_config(config_params)
     _check_with_config(sul, config_params)
 
 
 @pytest.mark.parametrize(("name", "sequence_number"), (("SUL-1", "11"), ("Default storage", "0")))
-def test_from_config_with_params(name, sequence_number):
+def test_from_config_with_params(name: str, sequence_number: str):
+    """Test creating StorageUnitLabel from config with alternative parameters."""
+
     config = make_config("StorageUnitLabel")
     config["StorageUnitLabel"]["name"] = name
     config["StorageUnitLabel"]["sequence_number"] = sequence_number
