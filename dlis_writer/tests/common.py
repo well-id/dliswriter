5ff51ae8791c6a2effcbba421ae9247ecec0f0d8
DominikaDlugosz <dominika.dlugosz@wellid.no>
2023-12-04 09:46:10 +0100

    [Docstrings] Common helper funcs and fixtures for all tests

--- dlis_writer/tests/common.py
+++ dlis_writer/tests/common.py
@@ -7,6 +7,8 @@ from dlis_writer.logical_record.eflr_types import eflr_types
 
 
 def clear_eflr_instance_registers():
+    """Remove all defined instances of EFLR from the internal dicts. Clear the EFLRObject dicts of the instances."""
+
     for eflr_type in eflr_types:
         for eflr in eflr_type.get_all_instances():
             eflr.clear_object_dict()
@@ -14,16 +16,22 @@ def clear_eflr_instance_registers():
 
 
 @pytest.fixture(scope='session')
-def base_data_path():
+def base_data_path() -> Path:
+    """Path to the resources files."""
+
     return Path(__file__).resolve().parent
 
 
 @pytest.fixture(scope='session')
-def config_params(base_data_path):
+def config_params(base_data_path: Path) -> ConfigParser:
+    """Config object with information on different DLIS objects to be included."""
+
     return load_config(base_data_path/'resources/mock_config_params.ini')
 
 
-def make_config(*sections):
+def make_config(*sections: str) -> ConfigParser:
+    """Create a config object containing all sections whose names are specified as positional args."""
+
     config = ConfigParser()
     for section in sections:
         config.add_section(section)
--- dlis_writer/tests/test_dlis_writer.py
+++ dlis_writer/tests/test_dlis_writer.py
@@ -2,4 +2,6 @@ import dlis_writer
 
 
 def test_version():
+    """Check version of the package."""
+
     assert dlis_writer.__version__ is not None
