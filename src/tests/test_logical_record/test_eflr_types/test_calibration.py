59dffb06084403944b4e327e761e35dffffbe38b
mysh <mysh995@gmail.com>
2023-12-14 09:48:30 +0100

    [Tests] Direct init: Calibration, CalibrationMeasurement, CalibrationItem

--- src/tests/test_logical_record/test_eflr_types/test_calibration.py
+++ src/tests/test_logical_record/test_eflr_types/test_calibration.py
@@ -1,7 +1,6 @@
+import pytest
 from datetime import datetime
-from configparser import ConfigParser
 
-from dlis_writer.logical_record.eflr_types import CalibrationMeasurementSet, CalibrationCoefficientSet, CalibrationSet
 from dlis_writer.logical_record.eflr_types.calibration import (CalibrationMeasurementItem,
                                                                CalibrationCoefficientItem, CalibrationItem)
 from dlis_writer.logical_record.eflr_types.channel import ChannelItem
@@ -9,14 +8,73 @@ from dlis_writer.logical_record.eflr_types.axis import AxisItem
 from dlis_writer.logical_record.eflr_types.parameter import ParameterItem
 from dlis_writer.logical_record.core.attribute import EFLRAttribute
 
-from tests.common import base_data_path, config_params
+
+@pytest.fixture
+def channel1():
+    return ChannelItem("Channel 1")
 
 
-def test_calibration_measurement_from_config(config_params: ConfigParser):
-    """Check that a CalibrationMeasurementObject is correctly set up from config info."""
+@pytest.fixture
+def channel2():
+    return ChannelItem("Channel 2")
 
-    key = "CalibrationMeasurement-1"
-    m: CalibrationMeasurementItem = CalibrationMeasurementItem.from_config(config_params, key=key)
+
+@pytest.fixture
+def channel3():
+    return ChannelItem("Channel 3")
+
+
+@pytest.fixture
+def ccoef1():
+    return CalibrationCoefficientItem("COEF-1")
+
+
+@pytest.fixture
+def cmeasure1():
+    return CalibrationMeasurementItem("CMEASURE-1")
+
+
+@pytest.fixture
+def axis1():
+    return AxisItem("Axis-1")
+
+
+@pytest.fixture
+def param1():
+    return ParameterItem("Param-1")
+
+
+@pytest.fixture
+def param2():
+    return ParameterItem("Param-2")
+
+
+@pytest.fixture
+def param3():
+    return ParameterItem("Param-3")
+
+
+
+def test_calibration_measurement_creation(channel1, axis1):
+    m = CalibrationMeasurementItem(
+        "CMEASURE-1",
+        **{
+            'phase': 'BEFORE',
+            'measurement_source': channel1,
+            '_type': 'Plus',
+            'axis.value': axis1,
+            'measurement.value': 12.2323,
+            'sample_count.value': 12,
+            'maximum_deviation': 2.2324,
+            'begin_time': '2050/03/12 12:30:00',
+            'duration': 15,
+            'duration.units': 's',
+            'reference': [11],
+            'standard.value': [11.2],
+            'plus_tolerance': [2],
+            'minus_tolerance.value': 1
+        }
+    )
 
     assert m.name == "CMEASURE-1"
     assert m.phase.value == 'BEFORE'
@@ -38,11 +96,17 @@ def test_calibration_measurement_from_config(config_params: ConfigParser):
     assert m.minus_tolerance.value == [1]
 
 
-def test_calibration_coefficient_from_config(config_params: ConfigParser):
+def test_calibration_coefficient_creation():
     """Check that a CalibrationCoefficientObject is correctly set up from config info."""
 
-    key = "CalibrationCoefficient-1"
-    c: CalibrationCoefficientItem = CalibrationCoefficientItem.from_config(config_params, key=key)
+    c = CalibrationCoefficientItem(
+        'COEF-1',
+        label='Gain',
+        coefficients=[100.2, 201.3],
+        references=[89, 298],
+        plus_tolerances=[100.2, 222.124],
+        minus_tolerances=[87.23, 214],
+    )
 
     assert c.name == "COEF-1"
     assert c.label.value == 'Gain'
@@ -64,16 +128,22 @@ def _check_list(objects: EFLRAttribute, names: tuple[str, ...], object_class: ty
         assert objects[i].name == name
 
 
-def test_calibration_from_config(config_params: ConfigParser):
+def test_calibration_creation(channel1, channel2, channel3, ccoef1, cmeasure1, param1, param2, param3):
     """Check that a CalibrationObject is correctly set up from config info."""
 
-    key = "Calibration-1"
-    c: CalibrationItem = CalibrationItem.from_config(config_params, key=key)
+    c = CalibrationItem(
+        "CALIB-MAIN",
+        calibrated_channels=(channel1, channel2),
+        uncalibrated_channels=(channel3,),
+        coefficients=(ccoef1,),
+        measurements=(cmeasure1,),
+        parameters=(param1, param2, param3)
+    )
 
     assert c.name == "CALIB-MAIN"
 
     _check_list(c.calibrated_channels, ("Channel 1", "Channel 2"), ChannelItem)
-    _check_list(c.uncalibrated_channels, ("amplitude", "radius", "radius_pooh"), ChannelItem)
+    _check_list(c.uncalibrated_channels, ("Channel 3",), ChannelItem)
     _check_list(c.coefficients, ("COEF-1",), CalibrationCoefficientItem)
     _check_list(c.measurements, ("CMEASURE-1",), CalibrationMeasurementItem)
     _check_list(c.parameters, ("Param-1", "Param-2", "Param-3"), ParameterItem)
