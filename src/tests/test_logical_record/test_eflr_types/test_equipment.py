4084511e51bde0389c0dc1a7467a636642281480
mysh <mysh995@gmail.com>
2023-12-14 10:29:09 +0100

    [Tests] Direct init: Equipment

--- src/tests/test_logical_record/test_eflr_types/test_equipment.py
+++ src/tests/test_logical_record/test_eflr_types/test_equipment.py
@@ -1,20 +1,21 @@
 import pytest
-from configparser import ConfigParser
 
 from dlis_writer.logical_record.eflr_types.equipment import EquipmentSet, EquipmentItem
 
-from tests.common import base_data_path, config_params
 
-
-@pytest.mark.parametrize(("idx", "section", "name", "status", "serial_number"), (
-        (0, "Equipment-1", "EQ1", 1, "9101-21391"),
-        (1, "Equipment-2", "EQ2", 0, "5559101-21391"),
-        (2, "Equipment-X", "EqX", 1, "12311")
+@pytest.mark.parametrize(("name", "status", "serial_number"), (
+        ("EQ1", 1, "9101-21391"),
+        ("EQ2", 0, "5559101-21391"),
+        ("EqX", 1, "12311")
 ))
-def test_from_config(config_params: ConfigParser, idx: int, section: str, name: str, status: int, serial_number: str):
+def test_creation(name: str, status: int, serial_number: str):
     """Check that EquipmentObject instances are correctly created from config."""
 
-    eq: EquipmentItem = EquipmentItem.from_config(config_params, key=section)
+    eq = EquipmentItem(
+        name,
+        status=status,
+        serial_number=serial_number
+    )
 
     assert eq.name == name
     assert eq.status.value == status
@@ -22,11 +23,40 @@ def test_from_config(config_params: ConfigParser, idx: int, section: str, name: 
     assert eq.serial_number.value == serial_number
     assert isinstance(eq.serial_number.value, str)
 
+    assert isinstance(eq.parent, EquipmentSet)
+    assert eq.parent.set_name is None
 
-def test_from_config_params_and_units(config_params: ConfigParser):
-    """Check setting up EquipmentObject's parameters and units from config."""
 
-    eq: EquipmentItem = EquipmentItem.from_config(config_params, key="Equipment-1")
+def test_params_and_units():
+    """Check setting up EquipmentObject's parameters and units."""
+
+    eq = EquipmentItem(
+        "Equipment-1",
+        **{
+            'height': 140,
+            'height.units': 'in',
+            'length': 230.78,
+            'length.units': 'cm',
+            'minimum_diameter': 2.3,
+            'minimum_diameter.units': 'm',
+            'maximum_diameter': 3.2,
+            'maximum_diameter.units': 'm',
+            'weight': 1.2,
+            'weight.units': 't',
+            'hole_size': 323.2,
+            'hole_size.units': 'm',
+            'pressure': 18000,
+            'pressure.units': 'psi',
+            'temperature': 24,
+            'temperature.units': 'degC',
+            'vertical_depth': 587,
+            'vertical_depth.units': 'm',
+            'radial_drift': 23.22,
+            'radial_drift.units': 'm',
+            'angular_drift': 32.5,
+            'angular_drift.units': 'm'
+        }
+    )
 
     def check(name, val, unit):
         attr = getattr(eq, name)
