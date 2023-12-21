3414bccfeff84dda549c15cfd61da8250d59e358
mysh <mysh995@gmail.com>
2023-12-21 17:24:39 +0100

    [Tests] DictDataWrapper: dtypes

--- src/tests/test_utils/test_source_data_wrappers/test_dict_data_wrapper.py
+++ src/tests/test_utils/test_source_data_wrappers/test_dict_data_wrapper.py
@@ -16,12 +16,20 @@ def data():
     return d
 
 
+def test_basic_properties(data):
+    w = DictDataWrapper(data)
+    assert w.data_source is data
+    assert w.n_rows == 100
+    assert isinstance(w.dtype, np.dtype)
+    assert len(w.dtype) == 3
+
+
 def test_creation_without_mapping(data):
     w = DictDataWrapper(data)
 
     assert w.dtype.names == ('depth', 'rpm', 'amplitude')
     assert w.dtype[0] == np.float64
-    assert w.dtype[1] == np.int_
+    assert w.dtype[1] == np.int32
     assert w.dtype[2] == (np.float16, (128,))
 
 
@@ -30,5 +38,25 @@ def test_creation_with_mapping(data):
 
     assert w.dtype.names == ('MD', 'RPM', 'AMP')
     assert w.dtype[0] == np.float64
-    assert w.dtype[1] == np.int_
+    assert w.dtype[1] == np.int32
     assert w.dtype[2] == (np.float16, (128,))
+
+
+def test_creation_with_known_dtypes(data):
+    w = DictDataWrapper(data, known_dtypes={'depth': np.float32, 'rpm': np.float64})
+
+    assert w.dtype[0] == np.float32
+    assert w.dtype[1] == np.float64
+    assert w.dtype[2] == (np.float16, (128,))
+
+
+def test_creation_with_known_dtypes_and_mapping(data):
+    w = DictDataWrapper(
+        data,
+        mapping={'MD': 'depth', 'RPM': 'rpm', 'AMP': 'amplitude'},
+        known_dtypes={'MD': np.float16, 'AMP': np.float64}  # names the same as keys of the mapping dict
+    )
+
+    assert w.dtype[0] == np.float16
+    assert w.dtype[1] == np.int32
+    assert w.dtype[2] == (np.float64, (128,))
