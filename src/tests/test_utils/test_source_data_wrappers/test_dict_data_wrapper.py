7b70599c30562507f6e7916ed3fc25559f1633f4
DominikaDlugosz <dominika.dlugosz@wellid.no>
2024-01-09 15:02:45 +0100

    [Tests] Dict data wrapper - docs and type hints

--- src/tests/test_utils/test_source_data_wrappers/test_dict_data_wrapper.py
+++ src/tests/test_utils/test_source_data_wrappers/test_dict_data_wrapper.py
@@ -2,12 +2,18 @@ import numpy as np
 from pathlib import Path
 import pytest
 import logging
+from typing import Union, Any
 
 from dlis_writer.utils.source_data_wrappers import DictDataWrapper, SourceDataWrapper
 
 
+source_data_type = dict[str, np.ndarray]
+
+
 @pytest.fixture
-def data():
+def data() -> source_data_type:
+    """Mock source data for dict data wrapper."""
+
     n = 100
     d = {
         'depth': np.arange(n) * 0.1,
@@ -18,15 +24,20 @@ def data():
     return d
 
 
-def test_basic_properties(data):
+def test_basic_properties(data: source_data_type) -> None:
+    """Check basic attributes of a dict data wrapper instance created with default settings."""
+
     w = DictDataWrapper(data)
     assert w.data_source is data
     assert w.n_rows == 100
     assert isinstance(w.dtype, np.dtype)
-    assert len(w.dtype) == 3
+    assert isinstance(w.dtype.names, tuple)
+    assert len(w.dtype.names) == 3
 
 
-def test_creation_without_mapping(data):
+def test_creation_without_mapping(data: source_data_type) -> None:
+    """Check that a dict wrapper with default mapping is created correctly."""
+
     w = DictDataWrapper(data)
 
     assert w.dtype.names == ('depth', 'rpm', 'amplitude')
@@ -35,7 +46,9 @@ def test_creation_without_mapping(data):
     assert w.dtype[2] == (np.float16, (128,))
 
 
-def test_creation_with_mapping(data):
+def test_creation_with_mapping(data: source_data_type) -> None:
+    """Check that a dict wrapper with alternative mapping is created correctly."""
+
     w = DictDataWrapper(data, mapping={'MD': 'depth', 'RPM': 'rpm', 'AMP': 'amplitude'})
 
     assert w.dtype.names == ('MD', 'RPM', 'AMP')
@@ -44,7 +57,9 @@ def test_creation_with_mapping(data):
     assert w.dtype[2] == (np.float16, (128,))
 
 
-def test_creation_with_mapping_omitting_dsets(data):
+def test_creation_with_mapping_omitting_dsets(data: source_data_type) -> None:
+    """Test creating a dict wrapper which omits some of the datasets found in the source data."""
+
     w = DictDataWrapper(data, mapping={'MD': 'depth', 'AMP': 'amplitude'})  # no rpm
 
     assert w.dtype.names == ('MD', 'AMP')
@@ -52,7 +67,9 @@ def test_creation_with_mapping_omitting_dsets(data):
     assert w.dtype[1] == (np.float16, (128,))
 
 
-def test_creation_with_known_dtypes(data):
+def test_creation_with_known_dtypes(data: source_data_type) -> None:
+    """Test creating a dict wrapper with some data types explicitly provided."""
+
     w = DictDataWrapper(data, known_dtypes={'depth': np.float32, 'rpm': np.float64})
 
     assert w.dtype[0] == np.float32
@@ -60,7 +77,9 @@ def test_creation_with_known_dtypes(data):
     assert w.dtype[2] == (np.float16, (128,))
 
 
-def test_creation_with_known_dtypes_and_mapping(data):
+def test_creation_with_known_dtypes_and_mapping(data: source_data_type) -> None:
+    """Test creating a dict wrapper with alternative mapping and provided (matching) data types."""
+
     w = DictDataWrapper(
         data,
         mapping={'MD': 'depth', 'RPM': 'rpm', 'AMP': 'amplitude'},
@@ -73,13 +92,17 @@ def test_creation_with_known_dtypes_and_mapping(data):
 
 
 @pytest.mark.parametrize('data_dict', (np.arange(12), [10, 20, 30, 40], 'path/to/something'))
-def test_type_error_if_not_dict(data_dict):
+def test_type_error_if_not_dict(data_dict: Any) -> None:
+    """Check that a type error is raised if source data are not a dict."""
+
     with pytest.raises(TypeError, match="Expected a dictionary.*"):
         DictDataWrapper(data_dict)
 
 
 @pytest.mark.parametrize('radius', (3, object(), 'path/to/something', (1, 2, 4.5), list(range(100))))
-def test_type_error_if_not_numpy(data, radius):
+def test_type_error_if_not_numpy(data: source_data_type, radius: Any) -> None:
+    """Check that a type error is raised if values of the data dict are anything other than a numpy.ndarray."""
+
     data2 = data | {'radius': radius}
 
     with pytest.raises(TypeError, match='Dict values must be numpy arrays.*'):
@@ -87,26 +110,35 @@ def test_type_error_if_not_numpy(data, radius):
 
 
 @pytest.mark.parametrize('key', (3, Path(__file__).resolve(), (1, 2, 3)))
-def test_type_error_if_key_not_str(data, key):
+def test_type_error_if_key_not_str(data: source_data_type, key: Any) -> None:
+    """Check that a type error is raised if keys of the data dictionary are not strings."""
+
     data2 = data | {key: np.random.rand(100)}
 
     with pytest.raises(TypeError, match="Source dictionary keys must be strings.*"):
         DictDataWrapper(data2)
 
 
-def test_creation_from_superclass(data):
+def test_creation_from_superclass(data: source_data_type) -> None:
+    """Check that a dict wrapper is created from SourceDataWrapper.make_wrapper if the source data are a dict."""
+
     w = SourceDataWrapper.make_wrapper(data)
     assert isinstance(w, DictDataWrapper)
 
 
 @pytest.mark.parametrize(('from_idx', 'to_idx', 'n_rows'), ((0, 10, 10), (60, 63, 3), (92, None, 8)))
-def test_creation_with_from_and_to_idx(data, from_idx, to_idx, n_rows):
+def test_creation_with_from_and_to_idx(data: source_data_type, from_idx: int, to_idx: Union[int, None],
+                                       n_rows: int) -> None:
+    """Check creating a dict wrapper with specified from- and to-indices in the original data."""
+
     w = DictDataWrapper(data, from_idx=from_idx, to_idx=to_idx)
     assert w.n_rows == n_rows
 
 
 @pytest.mark.parametrize(('start', 'stop'), ((0, 10), (1, 2), (3, 3), (3, 17), (61, 100)))
-def test_load_chunk(data, start, stop):
+def test_load_chunk(data: source_data_type, start: int, stop: int) -> None:
+    """Test loading a data chunk from a dict wrapper."""
+
     w = DictDataWrapper(data)
     chunk = w.load_chunk(start, stop)
 
@@ -118,7 +150,9 @@ def test_load_chunk(data, start, stop):
 
 
 @pytest.mark.parametrize('start', (0, 10, 99, 100))
-def test_load_chunk_until_end(data, start):
+def test_load_chunk_until_end(data: source_data_type, start: int) -> None:
+    """Test loading a data chunk from a specified point until the end."""
+
     w = DictDataWrapper(data)
     chunk = w.load_chunk(start, stop=None)
 
@@ -130,7 +164,9 @@ def test_load_chunk_until_end(data, start):
 
 
 @pytest.mark.parametrize(('start', 'stop'), ((0, 101), (99, 1000)))
-def test_load_chunk_stop_too_large(data, start, stop):
+def test_load_chunk_stop_too_large(data: source_data_type, start: int, stop: int) -> None:
+    """Check that a ValueError is raised if the specified chunk stop index is too large."""
+
     w = DictDataWrapper(data)
 
     with pytest.raises(ValueError, match=f"Cannot load chunk up to row {stop}.*"):
@@ -138,7 +174,9 @@ def test_load_chunk_stop_too_large(data, start, stop):
 
 
 @pytest.mark.parametrize('start', (101, 1210))
-def test_load_chunk_start_too_large(data, start):
+def test_load_chunk_start_too_large(data: source_data_type, start: int) -> None:
+    """Check that a ValueError is raised if the specified chunk start index is too large."""
+
     w = DictDataWrapper(data)
 
     with pytest.raises(ValueError, match=f"Cannot load chunk from row {start}.*"):
@@ -146,7 +184,9 @@ def test_load_chunk_start_too_large(data, start):
 
 
 @pytest.mark.parametrize(('start', 'stop'), ((10, 9), (90, 10)))
-def test_load_chunk_start_larger_than_stop(data, start, stop):
+def test_load_chunk_start_larger_than_stop(data: source_data_type, start: int, stop: int) -> None:
+    """Check that a ValueError is raised if the specified chunk start is larger than the stop."""
+
     w = DictDataWrapper(data)
 
     with pytest.raises(ValueError, match=f"Stop row cannot be smaller than start row.*"):
@@ -154,7 +194,9 @@ def test_load_chunk_start_larger_than_stop(data, start, stop):
 
 
 @pytest.mark.parametrize(('start', 'from_idx'), ((60, 50), (10, 92), (91, 10)))
-def test_load_chunk_start_too_large_with_from_idx(data, start, from_idx):
+def test_load_chunk_start_too_large_with_from_idx(data: source_data_type, start: int, from_idx: int) -> None:
+    """Check that a ValueError is raised if the specified chunk start index is too large with a from_idx specified."""
+
     w = DictDataWrapper(data, from_idx=from_idx)
 
     with pytest.raises(ValueError, match=f"Cannot load chunk from row {start}.*"):
@@ -162,7 +204,9 @@ def test_load_chunk_start_too_large_with_from_idx(data, start, from_idx):
 
 
 @pytest.mark.parametrize(('stop', 'from_idx'), ((60, 50), (10, 92), (91, 10)))
-def test_load_chunk_stop_too_large_with_from_idx(data, stop, from_idx):
+def test_load_chunk_stop_too_large_with_from_idx(data: source_data_type, stop: int, from_idx: int) -> None:
+    """Check that a ValueError is raised if the specified chunk stop index is too large with a from_idx specified."""
+
     w = DictDataWrapper(data, from_idx=from_idx)
 
     with pytest.raises(ValueError, match=f"Cannot load chunk up to row {stop}.*"):
@@ -175,7 +219,10 @@ def test_load_chunk_stop_too_large_with_from_idx(data, stop, from_idx):
         (3, 3, 28, 55),
         (3, 17, 83, None)
 ))
-def test_load_chunk_with_from_and_to_idx(data, start, stop, from_idx, to_idx):
+def test_load_chunk_with_from_and_to_idx(data: source_data_type, start: int, stop: int, from_idx: int,
+                                         to_idx: Union[int, None]) -> None:
+    """Test loading a data chunk if from_idx and to_idx were specified."""
+
     w = DictDataWrapper(data, from_idx=from_idx, to_idx=to_idx)
     chunk = w.load_chunk(start, stop)
 
@@ -192,7 +239,10 @@ def test_load_chunk_with_from_and_to_idx(data, start, stop, from_idx, to_idx):
         (3, 28, 55),
         (3, 83, None)
 ))
-def test_load_chunk_until_end_with_from_and_to_idx(data, start, from_idx, to_idx):
+def test_load_chunk_until_end_with_from_and_to_idx(data: source_data_type, start: int, from_idx: int,
+                                                   to_idx: Union[int, None]) -> None:
+    """Test loading a data chunk until the end if from_idx and to_idx were specified."""
+
     w = DictDataWrapper(data, from_idx=from_idx, to_idx=to_idx)
     chunk = w.load_chunk(start, stop=None)
 
@@ -203,7 +253,9 @@ def test_load_chunk_until_end_with_from_and_to_idx(data, start, from_idx, to_idx
     assert (chunk['amplitude'] == data['amplitude'][from_idx+start:to_idx]).all()
 
 
-def test_load_chunk_alternative_mapping(data):
+def test_load_chunk_alternative_mapping(data: source_data_type) -> None:
+    """Test loading a data chunk if alternative data names mapping was provided."""
+
     w = DictDataWrapper(data, mapping={'AMP': 'amplitude', 'MD': 'depth'})  # no rpm, switched order
     chunk = w.load_chunk(10, 20)
 
@@ -216,7 +268,10 @@ def test_load_chunk_alternative_mapping(data):
 
 
 @pytest.mark.parametrize(("from_idx", "to_idx", "n_rows"), ((0, None, 100), (20, 60, 40)))
-def test_chunked_generator_all_rows(data, caplog, from_idx, to_idx, n_rows):
+def test_chunked_generator_all_rows(data: source_data_type, caplog: pytest.LogCaptureFixture, from_idx: int,
+                                    to_idx: Union[int, None], n_rows: int) -> None:
+    """Test loading data in a single chunk."""
+
     w = DictDataWrapper(data, from_idx=from_idx, to_idx=to_idx)
 
     with caplog.at_level(logging.DEBUG, logger='dlis_writer'):
@@ -234,7 +289,10 @@ def test_chunked_generator_all_rows(data, caplog, from_idx, to_idx, n_rows):
 
 
 @pytest.mark.parametrize(("chunk_rows", "n_chunks"), ((10, 10), (25, 4)))
-def test_chunked_generator_full_chunks(data, caplog, chunk_rows, n_chunks):
+def test_chunked_generator_full_chunks(data: source_data_type, caplog: pytest.LogCaptureFixture, chunk_rows: int,
+                                       n_chunks: int) -> None:
+    """Test loading data in several full chunks."""
+
     w = DictDataWrapper(data)
 
     with caplog.at_level(logging.DEBUG, logger='dlis_writer'):
@@ -255,7 +313,10 @@ def test_chunked_generator_full_chunks(data, caplog, chunk_rows, n_chunks):
 
 
 @pytest.mark.parametrize(("chunk_rows", "n_full_chunks", "remainder_rows"), ((15, 6, 10), (40, 2, 20), (60, 1, 40)))
-def test_chunked_generator_remainder_chunk(data, caplog, chunk_rows, n_full_chunks, remainder_rows):
+def test_chunked_generator_remainder_chunk(data: source_data_type, caplog: pytest.LogCaptureFixture, chunk_rows: int,
+                                           n_full_chunks: int, remainder_rows: int) -> None:
+    """Test loading data in several full and last half-full chunk."""
+
     w = DictDataWrapper(data)
 
     with caplog.at_level(logging.DEBUG, logger='dlis_writer'):
@@ -276,7 +337,9 @@ def test_chunked_generator_remainder_chunk(data, caplog, chunk_rows, n_full_chun
 
 
 @pytest.mark.parametrize(("from_idx", "to_idx"), ((0, None), (10, 30)))
-def test_getitem_default_mapping(data, from_idx, to_idx):
+def test_getitem_default_mapping(data: source_data_type, from_idx: int, to_idx: Union[int, None]) -> None:
+    """Test the __getitem__ functionality for a dict wrapper created with default mapping."""
+
     w = DictDataWrapper(data, from_idx=from_idx, to_idx=to_idx)
 
     for key in ('depth', 'amplitude', 'rpm'):
@@ -288,7 +351,9 @@ def test_getitem_default_mapping(data, from_idx, to_idx):
 
 
 @pytest.mark.parametrize(("from_idx", "to_idx"), ((0, None), (20, 21)))
-def test_getitem_alternative_mapping(data, from_idx, to_idx):
+def test_getitem_alternative_mapping(data: source_data_type, from_idx: int, to_idx: Union[int, None]) -> None:
+    """Test the __getitem__ functionality for a dict wrapper created with alternative mapping."""
+
     w = DictDataWrapper(data, from_idx=from_idx, to_idx=to_idx, mapping={'MD': 'depth', 'RPM': 'rpm'})
 
     for key in ('MD', 'RPM'):
