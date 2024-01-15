cab24e55ab1c0545714a77a74518903cf2288298
DominikaDlugosz <dominika.dlugosz@wellid.no>
2024-01-15 12:33:18 +0100

    Renamed and documented the equivalent (parallel) frames example

--- examples/create_dlis_equivalent_frames.py
+++ examples/create_dlis_equivalent_frames.py
@@ -1,8 +1,18 @@
+"""This is an example of how to create a DLIS file with multiple frames with equivalent channels - i.e. the same channel
+names used in different frames (but referring to separate datasets).
+
+The procedure is the same as for standard DLIS files.
+When inspecting the objects, one might notice that:
+    - copy_number of the channels is different - 0 for channels of frame 1, 1 for channels of frame 2
+    - dataset_name attributes of the corresponding channels are also different, so that datasets are not overwritten.
+"""
+
 import numpy as np
 import logging
 
 from dlis_writer.file import DLISFile
-from dlis_writer.logical_record.eflr_types.origin import OriginItem
+from dlis_writer.logical_record.core.eflr import EFLRItem
+from dlis_writer.logical_record.eflr_types import OriginItem, ChannelItem
 from dlis_writer.utils.logging import install_logger
 
 
@@ -38,3 +48,17 @@ frame2 = df.add_frame("FRAME2", channels=(ch_depth_2, ch_rpm_2, ch_amp_2), index
 
 # write the file
 df.write('./tmp.DLIS')
+
+
+# print the copy_number and dataset_name info of the objects for inspection
+def describe_item(o: EFLRItem):
+    s = f"{o}: \n\tcopy_number = {o.copy_number}"
+
+    if isinstance(o, ChannelItem):
+        s += f"\n\tdataset_name = {o.dataset_name}"
+
+    print(s)
+
+
+for eflr_item in (frame1, ch_depth_1, ch_rpm_1, ch_amp_1, frame2, ch_depth_2, ch_rpm_2, ch_amp_2):
+    describe_item(eflr_item)
