ce1692ad9a57f6083caf6fdd75b9d6dc97bd3dde
DominikaDlugosz <dominika.dlugosz@wellid.no>
2024-01-15 11:01:25 +0100

    Parallel frames example

---
+++ examples/create_dlis_parallel_frames.py
@@ -0,0 +1,47 @@
+import numpy as np
+import logging
+
+from dlis_writer.file import DLISFile
+from dlis_writer.logical_record.eflr_types.origin import OriginItem
+from dlis_writer.utils.logging import install_logger
+
+
+# colored logs output
+logger = logging.getLogger(__name__)
+install_logger(logger)
+
+
+# set up origin & file header with custom parameters - by creating an instance or dict of kwargs
+origin = OriginItem("DEFAULT ORIGIN", file_set_number=1, company="XXX")
+file_header = {'sequence_number': 2}
+
+
+# create DLISFile instance, pass the origin and file header
+df = DLISFile(origin=origin, file_header=file_header)
+
+
+# define frame 1
+n_rows_1 = 100
+ch_depth_1 = df.add_channel('DEPTH', data=np.arange(n_rows_1), units='m', set_name='1')
+ch_rpm_1 = df.add_channel("RPM", data=10 * np.random.rand(n_rows_1), set_name='1')
+ch_amp_1 = df.add_channel("AMPLITUDE", data=np.random.rand(n_rows_1, 5), set_name='1')
+frame1 = df.add_frame("FRAME1", channels=(ch_depth_1, ch_rpm_1, ch_amp_1), index_type='BOREHOLE-DEPTH')
+
+
+for ch in (ch_depth_1, ch_rpm_1, ch_amp_1):
+    ch.copy_number = 1
+
+
+# define frame 2
+n_rows_2 = 200
+ch_depth_2 = df.add_channel('DEPTH', data=np.arange(n_rows_2), units='m', set_name='2', dataset_name='depth2')
+ch_rpm_2 = df.add_channel("RPM", data=(np.arange(n_rows_2) % 10).astype(np.int32), set_name='2', dataset_name='rpm2')
+ch_amp_2 = df.add_channel("AMPLITUDE", data=np.arange(n_rows_2 * 5).reshape(n_rows_2, 5) % 6, set_name='2', dataset_name='amp2')
+frame2 = df.add_frame("FRAME2", channels=(ch_depth_2, ch_rpm_2, ch_amp_2), index_type='BOREHOLE-DEPTH')
+
+for ch in (ch_depth_2, ch_rpm_2, ch_amp_2):
+    ch.copy_number = 2
+
+
+# write the file
+df.write('./tmp.DLIS')
