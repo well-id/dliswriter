e37246c440ed1258ed1afc5172428baf21d20a94
DominikaDlugosz <dominika.dlugosz@wellid.no>
2024-01-16 15:06:23 +0100

    Added classes for direct import from the package root

--- src/dlis_writer/__init__.py
+++ src/dlis_writer/__init__.py
@@ -1,1 +1,10 @@
+from dlis_writer.file.file import DLISFile
+from dlis_writer.logical_record.core.eflr import EFLRSet, EFLRItem
+from dlis_writer.logical_record.core.attribute import *
+from dlis_writer.logical_record.eflr_types import *
+from dlis_writer.logical_record.misc.storage_unit_label import StorageUnitLabel
+from dlis_writer.utils.enums import RepresentationCode, UNITS
+from dlis_writer.utils.source_data_wrappers import SourceDataWrapper, DictDataWrapper, NumpyDataWrapper, HDF5DataWrapper
+
+
 __version__ = '0.0.1'
