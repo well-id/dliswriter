099ff9932d8d0e4479fa5052e7bc770785e78874
DominikaDlugosz <dominika.dlugosz@wellid.no>
2024-04-08 14:09:51 +0200

    Separated calibration tests acc. to tested objects

---
+++ src/tests/test_logical_record/test_eflr_types/test_calibration_coefficient.py
@@ -0,0 +1,69 @@
+import pytest
+
+from dlis_writer import high_compatibility_mode_decorator
+from dlis_writer.logical_record.eflr_types import CalibrationCoefficientItem, CalibrationCoefficientSet
+
+
+def test_calibration_coefficient_creation() -> None:
+    """Check that a CalibrationCoefficientObject is correctly set up from config info."""
+
+    c = CalibrationCoefficientItem(
+        'COEF-1',
+        label='Gain',
+        coefficients=[100.2, 201.3],
+        references=[89, 298],
+        plus_tolerances=[100.2, 222.124],
+        minus_tolerances=[87.23, 214],
+        parent=CalibrationCoefficientSet()
+    )
+
+    assert c.name == "COEF-1"
+    assert c.label.value == 'Gain'
+    assert c.coefficients.value == [100.2, 201.3]
+    assert c.references.value == [89, 298]
+    assert c.plus_tolerances.value == [100.2, 222.124]
+    assert c.minus_tolerances.value == [87.23, 214]
+
+
+def test_calibration_coefficient_unequal_counts() -> None:
+
+    c = CalibrationCoefficientItem(
+        'CX',
+        label='Offset',
+        coefficients=[100.2, 201.3],
+        references=[89, 298, 21],
+        plus_tolerances=[100.2, 222.124],
+        parent=CalibrationCoefficientSet()
+    )
+
+    with pytest.raises(RuntimeError,
+                       match="Number of values all numeric attributes of Calibration Coefficient should be equal; "
+                             "got 2 for COEFFICIENTS, 3 for REFERENCES, 2 for PLUS-TOLERANCES"):
+        c.make_item_body_bytes()
+
+
+@pytest.mark.parametrize("label", ("CCOEF-1", "COEFFICIENT_3", "3112-1212"))
+@high_compatibility_mode_decorator
+def test_ccoef_label_compatible(label: str) -> None:
+    CalibrationCoefficientItem("CC-1", label=label, parent=CalibrationCoefficientSet())
+
+
+@pytest.mark.parametrize("label", ("Calibration coefficient", "CALIB C0EFF-3", "C@2"))
+@high_compatibility_mode_decorator
+def test_ccoef_label_not_compatible(label: str) -> None:
+    with pytest.raises(ValueError, match=".*strings can contain only uppercase characters, digits, dashes, .*"):
+        CalibrationCoefficientItem("CC-1", label=label, parent=CalibrationCoefficientSet())
+
+
+@pytest.mark.parametrize("name", ("CCOEF-1", "COEFFICIENT_3", "C12"))
+@high_compatibility_mode_decorator
+def test_ccoef_name_compatible(name: str) -> None:
+    CalibrationCoefficientItem(name, parent=CalibrationCoefficientSet())
+
+
+@pytest.mark.parametrize("name", ("Coefficient", "C0EFF 3", "C.12"))
+@high_compatibility_mode_decorator
+def test_ccoef_label_not_compatible(name: str) -> None:
+    with pytest.raises(ValueError, match=".*strings can contain only uppercase characters, digits, dashes, .*"):
+        CalibrationCoefficientItem(name, parent=CalibrationCoefficientSet())
+
---
+++ src/tests/test_logical_record/test_eflr_types/test_calibration_measurement.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+from dlis_writer import AttrSetup, enums
+from dlis_writer.logical_record.eflr_types import (AxisItem, ChannelItem, CalibrationMeasurementItem,
+                                                   CalibrationMeasurementSet)
+
+
+def test_calibration_measurement_creation(channel1: ChannelItem, axis1: AxisItem) -> None:
+    m = CalibrationMeasurementItem(
+        "CMEASURE-1",
+        **{
+            'phase': enums.CalibrationMeasurementPhase.BEFORE,
+            'measurement_source': channel1,
+            'type': 'Plus',
+            'axis': axis1,
+            'measurement': AttrSetup(12.2323),
+            'sample_count': {'value': 12},
+            'maximum_deviation': 2.2324,
+            'begin_time': '2050/03/12 12:30:00',
+            'duration': AttrSetup(15, 's'),
+            'reference': [11],
+            'standard': [11.2],
+            'plus_tolerance': [2],
+            'minus_tolerance': AttrSetup(value=1),
+        },
+        parent=CalibrationMeasurementSet()
+    )
+
+    assert m.name == "CMEASURE-1"
+    assert m.phase.value == 'BEFORE'
+    assert isinstance(m.measurement_source.value, ChannelItem)
+    assert m.measurement_source.value.name == "Channel 1"
+    assert m.type.value == 'Plus'
+    assert isinstance(m.axis.value[0], AxisItem)
+    assert m.axis.value[0].name == 'Axis-1'
+    assert m.measurement.value == [12.2323]
+    assert m.sample_count.value == 12
+    assert m.maximum_deviation.value == [2.2324]
+    assert isinstance(m.begin_time.value, datetime)
+    assert m.begin_time.value == datetime(2050, 3, 12, 12, 30)
+    assert m.duration.value == 15
+    assert m.duration.units == 's'
+    assert m.reference.value == [11]
+    assert m.standard.value == [11.2]
+    assert m.plus_tolerance.value == [2]
+    assert m.minus_tolerance.value == [1]
