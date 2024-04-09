08c9a211ab8fcdbea88bf5278701f66f407ba4ea
DominikaDlugosz <dominika.dlugosz@wellid.no>
2024-04-09 13:20:01 +0200

    Test compatibility of name of SUL

--- src/tests/test_logical_record/test_misc/test_storage_unit_label.py
+++ src/tests/test_logical_record/test_misc/test_storage_unit_label.py
@@ -1,6 +1,7 @@
 import pytest
 
 from dlis_writer.logical_record.misc import StorageUnitLabel
+from dlis_writer import high_compatibility_mode_decorator
 
 
 @pytest.mark.parametrize(("name", "sequence_number"), (("SUL-1", 11), ("Default storage", 0)))
@@ -11,3 +12,16 @@ def test_sul_creation(name: str, sequence_number: int) -> None:
 
     assert sul.set_identifier == name
     assert sul.sequence_number == sequence_number
+
+
+@pytest.mark.parametrize("name", ("SUL-1", "UNIT1", "123-11"))
+@high_compatibility_mode_decorator
+def test_name_compatible(name: str) -> None:
+    StorageUnitLabel(name)
+
+
+@pytest.mark.parametrize("name", ("STORAGE UNIT LABEL", "SUL#12", "12.3"))
+@high_compatibility_mode_decorator
+def test_name_not_compatible(name: str) -> None:
+    with pytest.raises(ValueError, match=".*strings can contain only uppercase characters, digits, dashes, .*"):
+        StorageUnitLabel(name)
