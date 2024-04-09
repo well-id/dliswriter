8ce364d19737137c7d97ce39af427e83248f8775
DominikaDlugosz <dominika.dlugosz@wellid.no>
2024-04-09 13:05:58 +0200

    Test compatibility of file header id

--- src/tests/test_logical_record/test_eflr_types/test_file_header.py
+++ src/tests/test_logical_record/test_eflr_types/test_file_header.py
@@ -1,6 +1,7 @@
 import pytest
 
 from dlis_writer.logical_record.eflr_types.file_header import FileHeaderSet, FileHeaderItem
+from dlis_writer import high_compatibility_mode_decorator
 
 
 @pytest.mark.parametrize(("header_id", "identifier", "sequence_number"), (("a", "A", 9), ("123ert", "8", 3)))
@@ -14,3 +15,17 @@ def test_creation(header_id: str, identifier: str, sequence_number: int) -> None
 
     assert isinstance(fh.parent, FileHeaderSet)
     assert fh.parent.set_name is None
+
+
+@pytest.mark.parametrize("name", ("FH-1", "DEFAULT_FILE_HEADER", "12-3"))
+@high_compatibility_mode_decorator
+def test_header_id_compatible(name: str) -> None:
+    FileHeaderItem(name, parent=FileHeaderSet())
+
+
+@pytest.mark.parametrize("name", ("Fh1", "file.header", "fh 8"))
+@high_compatibility_mode_decorator
+def test_header_id_not_compatible(name: str) -> None:
+    with pytest.raises(ValueError, match=".*strings can contain only uppercase characters, digits, dashes, .*"):
+        FileHeaderItem(name, parent=FileHeaderSet())
+
