7d9e649ab2cba349e20b45e488907ee444a21ac1
DominikaDlugosz <dominika.dlugosz@wellid.no>
2024-04-23 11:10:18 +0200

    Changed docs theme to renku

--- docs/conf.py
+++ docs/conf.py
@@ -25,8 +25,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'renku'
 html_static_path = ['_static']
+html_logo = "../logo.png"
 
 # Add an "Edit on GitHub" link to the top of all pages
 html_context = {
